ae52923e30f139a0a23341d7722042e55c903d89
AleMurguia <l20160062@culiacan.tecnm.mx>
2024-02-24 22:41:10 -0600

    1.0 homework

---
+++ Arbol Binario de Busqueda/src/Node.java
@@ -0,0 +1,10 @@
+// Clase para representar cada nodo del árbol.
+class Node {
+    int value;
+    Node left, right;
+
+    public Node(int item) {
+        value = item;
+        left = right = null;
+    }
+}
